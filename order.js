f2f8409c360025f655dfeadd828c6974362b130d
athr32 <143409216+athr32@users.noreply.github.com>
2024-07-22 08:24:24 +0530

    Delete order.js

--- order.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.food-item input[type="checkbox"]').forEach(checkbox => {
-        checkbox.addEventListener('change', showOrderSummary);
-    });
-
-    document.querySelectorAll('.quantity-controls input').forEach(input => {
-        input.addEventListener('change', showOrderSummary);
-    });
-});
