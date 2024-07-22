6b99618d330961aa782def591fb1f5e537ec9684
athr32 <143409216+athr32@users.noreply.github.com>
2024-07-22 08:23:34 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const username = localStorage.getItem('username');
-    if (username) {
-        document.getElementById('user_info').textContent = username;
-    } else {
-        // Redirect to login if not logged in
-        window.location.href = 'login.html';
-    }
-});
